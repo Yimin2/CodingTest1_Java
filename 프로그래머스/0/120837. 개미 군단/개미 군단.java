b57b77c9f57e24f8d91f8b086245106d83025b51
Yimin2 <100354143+Yimin2@users.noreply.github.com>
2024-04-19 11:25:42 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int hp) {
         int a,b,c, answer = 0;
         a = hp/5; 
-        b = (hp - a*5)/3;
-        c = (hp - a*5 - b*3);
+        b = (hp%5)/3;
+        c = (hp%5%3);
         answer = a+b+c;
         return answer;
     }
