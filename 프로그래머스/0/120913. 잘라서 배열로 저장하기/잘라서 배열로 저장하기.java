7c42a0a0ecc6fbc929333559b6b53d91a13aa34b
Yimin2 <100354143+Yimin2@users.noreply.github.com>
2024-07-01 11:31:27 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.06 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String my_str, int n) {
+        List<String> answer = new ArrayList<>();
+        
+        for (int i = 0; i < my_str.length(); i += n) {
+          int endIndex = Math.min(i+n,my_str.length());
+           answer.add(my_str.substring(i, endIndex));
+        }
+        return answer.toArray(new String[0]);
+    }
+}
